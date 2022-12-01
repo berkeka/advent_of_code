1e0b2a9d9e30fdb351bb17e9e22772488eff687e
Berke Kalkan <berkekalkantr@gmail.com>
2022-12-01 19:25:56 +0300

    Add solution for step 2 of 1-2022

--- 2022/01/main.rb
+++ 2022/01/main.rb
@@ -5,6 +5,7 @@ foods_by_elves = input.split("\n\n")
 
 index = -1
 max_calories = 0
+max_3_calories = [0, 0, 0]
 
 foods_by_elves.each_with_index do |foods_str, i|
   foods = foods_str.split("\n")
@@ -14,6 +15,18 @@ foods_by_elves.each_with_index do |foods_str, i|
     index = i
     max_calories = sum_of_calories
   end
+
+  if max_3_calories[0] < sum_of_calories
+    max_3_calories.unshift(sum_of_calories)
+    max_3_calories.pop
+  elsif max_3_calories[1] < sum_of_calories
+    max_3_calories[2] = max_3_calories[1]
+    max_3_calories[1] = sum_of_calories
+  elsif max_3_calories[2] < sum_of_calories
+    max_3_calories[2] = sum_of_calories
+  end
 end
 
 p max_calories
+
+p max_3_calories.sum
