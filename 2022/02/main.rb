ad4fe0e259143d098304f4a09f48e2982547e2b2
Berke Kalkan <berkekalkantr@gmail.com>
2022-12-02 20:19:10 +0300

    Add solution for step 2 of 2-2022

--- 2022/02/main.rb
+++ 2022/02/main.rb
@@ -5,7 +5,7 @@ rounds = input.split("\n")
 
 OUTCOME_SCORES = {
   # Rock
-  'X' => { 
+  'X' => {
     'A' => 3,
     'B' => 0,
     'C' => 6,
@@ -24,10 +24,49 @@ OUTCOME_SCORES = {
   },
 }
 
+REVERSED_OUTCOME_SCORES = {
+  # Rock
+  'X' => {
+    'A' => 3,
+    'B' => 6,
+    'C' => 0,
+  },
+  # Paper
+  'Y' => {
+    'A' => 0,
+    'B' => 3,
+    'C' => 6,
+  },
+  # Scissors
+  'Z' => {
+    'A' => 6,
+    'B' => 0,
+    'C' => 3,
+  },
+}
+
 SELECTION_SCORES = {
   'X' => 1,
   'Y' => 2,
   'Z' => 3,
+}
+
+DECRYPTION = {
+  'A' => 'X',
+  'B' => 'Y',
+  'C' => 'Z',
+}
+
+ENCRYPTION = {
+  'X' => 'A',
+  'Y' => 'B',
+  'Z' => 'C',
+}
+
+WIN_SITUATION = {
+  'X' => 0,
+  'Y' => 3,
+  'Z' => 6,
 }
 
 sum_of_points = rounds.each.inject(0) do |sum, round|
@@ -36,5 +75,18 @@ sum_of_points = rounds.each.inject(0) do |sum, round|
   sum += (SELECTION_SCORES[us] + OUTCOME_SCORES[us][opponent])
 end
 
+sum_of_points_2 = rounds.each.inject(0) do |sum, round|
+  opponent, us = round.split(' ')
+  decrypted_opponent = DECRYPTION[opponent]
+
+  reversed = REVERSED_OUTCOME_SCORES[decrypted_opponent].map(&:reverse).to_h
+
+  selection = reversed[WIN_SITUATION[us]]
+
+  sum += (WIN_SITUATION[us] + SELECTION_SCORES[DECRYPTION[selection]])
+end
+
 p sum_of_points
+
+p sum_of_points_2
 
