a3822780779dcd3ea5b46dfd225165f90921db94
Berke Kalkan <berkekalkantr@gmail.com>
2022-12-10 22:28:35 +0300

    Add solution for step 2 of 8-2022
    Code is quite ugly. Need to refactor later

--- 2022/08/main.rb
+++ 2022/08/main.rb
@@ -12,71 +12,85 @@ grid.map!{|x| x.chars.map!(&:to_i)}
 
 def visible_up?(grid, height_of_tree, i, j)
   visible = false
-  for index_y in (0...i)
+  score = 0
+  for index_y in (i - 1).downto(0)
     if grid[index_y][j] < height_of_tree
+      score += 1
       visible = true
     else  
-      return false
+      return false, score + 1
     end
   end
 
-  return visible
+  return visible, score
 end
 
 def visible_down?(grid, height_of_tree, i, j)
   visible = false
+  score = 0
   for index_y in (i + 1...$y)
     if grid[index_y][j] < height_of_tree
+      score += 1
       visible = true
     else  
-      return false
+      return false, score + 1
     end
   end
 
-  return visible
-end
-
-def visible_left?(grid, height_of_tree, i, j)
-  visible = false
-  for index_x in (j + 1...$x)
-    if grid[i][index_x] < height_of_tree
-      visible = true
-    else
-      return false
-    end 
-  end
-
-  return visible
+  return visible, score
 end
 
 def visible_right?(grid, height_of_tree, i, j)
   visible = false
-  for index_x in (0...j)
+  score = 0
+  for index_x in (j + 1...$x)
     if grid[i][index_x] < height_of_tree
+      score += 1
       visible = true
     else
-      return false
+      return false, score + 1
+    end 
+  end
+
+  return visible, score
+end
+
+def visible_left?(grid, height_of_tree, i, j)
+  visible = false
+  score = 0
+  for index_x in (j - 1).downto(0)
+    if grid[i][index_x] < height_of_tree
+      score += 1
+      visible = true
+    else
+      return false, score + 1
     end
   end
 
-  return visible
+  return visible, score
 end
 
 def visible?(grid, i, j)
   height_of_tree = grid[i][j]
 
-  return visible_up?(grid, height_of_tree, i, j) || 
-    visible_down?(grid, height_of_tree, i, j) || 
-    visible_left?(grid, height_of_tree, i, j) || 
-    visible_right?(grid, height_of_tree, i, j)
+  up_visible, up_score = visible_up?(grid, height_of_tree, i, j)
+  down_visible, down_score = visible_down?(grid, height_of_tree, i, j)
+  left_visible, left_score = visible_left?(grid, height_of_tree, i, j)
+  right_visible, right_score = visible_right?(grid, height_of_tree, i, j)
+
+  return (up_visible || down_visible || left_visible || right_visible), (up_score * down_score * left_score * right_score)
 end
 
 visible_tree_count = edges_count
+max_scenic_score = 0
 
 for i in (1...grid.length - 1)
   for j in (1...grid[i].length - 1)
-    visible_tree_count += 1 if visible?(grid, i, j)
+    visible, scenic_score = visible?(grid, i, j)
+    visible_tree_count += 1 if visible
+    max_scenic_score = scenic_score if scenic_score > max_scenic_score
   end
 end
 
 p visible_tree_count
+p max_scenic_score
